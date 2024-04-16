61dcb6a6c94a40293af1ccf83b28cbd6f037f4e9
edvinbaggman <edvinbaggman@hotmail.se>
2024-04-16 19:03:46 +0200

    Fixed spacing between cards in search view

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,7 +6,10 @@ interface Props {
 
 const Card = ({ movie }: Props) => {
   return (
-    <Link href={`/details?i=${movie.imdbID}`} className='px-2 sm:px-0 w-screen'>
+    <Link
+      href={`/details?i=${movie.imdbID}`}
+      className='px-2 sm:px-0 w-screen sm:w-52'
+    >
       <div className='sm:w-52 border shadow-lg rounded sm:hover:scale-105 duration-300'>
         <div className='w-full h-96 sm:h-60 bg-neutral-300 shadow rounded-t overflow-hidden'>
           {movie.Poster != 'N/A' && (

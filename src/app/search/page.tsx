7bc1180d1939ee225668386e82da89a7bd550b0a
edvinbaggman <edvinbaggman@hotmail.se>
2024-04-18 09:32:19 +0200

    Remove redundant vertical scrollbar in search view

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -22,7 +22,7 @@ export default async function Search({ searchParams }: Props) {
       <div className='font-bold text-2xl'>Search results</div>
       <div className='font-bold'>Click on a movie to see more details</div>
       <div className='max-w-full sm:px-10'>
-        <div className='flex flex-col sm:flex-row overflow-y-scroll gap-2 pb-10 pt-5'>
+        <div className='flex flex-col sm:flex-row overflow-x-scroll gap-2 pt-5 pb-10 sm:px-5'>
           {search.map((movie) => {
             return <Card key={movie.imdbID} movie={movie} />;
           })}

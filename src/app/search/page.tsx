6fd9fc79b1b77bd24a1123c871bcb7b56ede83fd
edvinbaggman <edvinbaggman@hotmail.se>
2024-04-18 23:27:51 +0200

    Placed cards in grid instead of row

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -15,19 +15,19 @@ export default async function Search({ searchParams }: Props) {
   const search = await searchMovieByTitle(query, year);
 
   return (
-    <>
-      <div className='w-full flex align-left pt-2 px-2 sm:px-10'>
+    <div>
+      <div className='w-full flex align-left p-2'>
         <BackButton />
       </div>
-      <div className='font-bold text-2xl'>Search results</div>
-      <div className='font-bold'>Click on a movie to see more details</div>
-      <div className='max-w-full sm:px-10'>
-        <div className='flex flex-col sm:flex-row overflow-x-scroll gap-2 pt-5 pb-10 sm:px-5'>
-          {search.map((movie) => {
-            return <Card key={movie.imdbID} movie={movie} />;
-          })}
-        </div>
+      <div className='flex flex-col items-center mt-5'>
+        <div className='font-bold text-2xl'>Search results</div>
+        <div className='font-bold'>Click on a movie to see more details</div>
       </div>
-    </>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-3 py-5 sm:px-2'>
+        {search.map((movie) => {
+          return <Card key={movie.imdbID} movie={movie} />;
+        })}
+      </div>
+    </div>
   );
 }
